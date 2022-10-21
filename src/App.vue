435520d4d4040de7e3243dff35f35ba346b1716e
shaiMatz <shyshir@gmail.com>
2022-10-21 16:59:06 +0300

    123

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
@@ -11,20 +12,15 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 nav {
   padding: 30px;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  
 }
 </style>
