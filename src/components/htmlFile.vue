773403f7a594471918dcc06a63773b6b85ad7b32
shaiMatz <shyshir@gmail.com>
2022-10-28 23:09:59 +0300

    url for lang better

--- src/components/htmlFile.vue
+++ src/components/htmlFile.vue
@@ -20,7 +20,6 @@ export default {
     max-height: 750;
     display: flex;
     flex-direction: column;
-    // box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
     background-color: #f6f6f8;
 .classHtml{
     align-content: center;
